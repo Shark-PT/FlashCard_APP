2cbc7f7f76a6f2260498be1f5731a808a843feb3
SharkPT <jccreis@hotmail.com>
2023-12-02 12:36:34 +0000

    inicio cartão frontal

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-BACKGROUND_COLOR = "#B1DDC6"
+from tkinter import *
 
+BACKGROUND_COLOR = "#B1DDC6"
+FONT_NAME = "Ariel"
+
+window = Tk()
+window.title("Flash Cards")
+window.config(bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=500, height=500)
+front_card_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(100, 100, image=front_card_img)
+timer_text = canvas.create_text(10, 10, text="French", fill="black", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+
+
+
+window.mainloop()
